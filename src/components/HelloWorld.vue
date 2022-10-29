0c61cb6591710486fe6b7557d52e8770f3307416
createrwebs <32156893+createrwebs@users.noreply.github.com>
2022-10-29 14:43:25 +0700

    chage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,23 +6,7 @@
         </v-flex>
 
         <v-flex mb-4>
-            <h1 class="display-2 font-weight-bold mb-3">
-                Welcome to NonMQL Expert Advisor Studio
-            </h1>
-            <h3 class="font-weight-bold mb-3">
-                Powered by Fintechee
-            </h3>
-            <p class="subheading font-weight-regular">
-                For help and collaboration with other Fintechee developers,
-                <br>please join our online
-                <a href="https://twitter.com/Fintechee1" target="_blank">Fintechee Twitter Community</a>.
-                <br>You can run Expert Advisor(EA) on our WEB Trader, a Forex trading platform
-                <a href="https://wwww.fintechee.com/web-trader/" target="_blank">Fintechee(the best Forex trading platform)</a>.
-                <br>Please refer to our tutorials to know more details
-                <a href="https://wwww.fintechee.com/tutorial-for-forex-trading/" target="_blank">Fintechee Tutorials</a>.
-                <br>Or check our SDK documents
-                <a href="https://wwww.fintechee.com/sdk-trading/" target="_blank">Fintechee SDK</a>.
-            </p>
+         
         </v-flex>-->
 
         <v-flex mb-5 xs12>
